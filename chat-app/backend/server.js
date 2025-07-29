8ed48711f5750029216348ff9895e8b4945836d9
Nishank Mishra <115864595+nishankmishra@users.noreply.github.com>
2025-07-29 12:08:22 +0530

    Update server.js

--- chat-app/backend/server.js
+++ chat-app/backend/server.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: process.env.port || 8080 },() => {
+const wss = new WebSocket.Server({ port: process.env.PORT || 8080 },() => {
   console.log('WebSocket server is running on ws://localhost:8080');
 });
 
